5393b08f7469370c707d35041475e0b2fbedc3bb
Zeno Rawling <zeno.rawling@gmail.com>
2014-02-01 17:36:47 +0000

    Initial commit. rudimentary table setup and a pair of flippers

---
+++ Assets/scripts/Flipper.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flipper : MonoBehaviour {
+
+	public bool right;
+
+	public float maxAngle = 60f;
+	public float speed = 100f;
+
+	public HingeJoint hingeJoint;
+
+	private JointSpring spring = new JointSpring();
+
+	float _targetAngle;
+
+	void Awake () {
+		hingeJoint.useSpring = true;
+	}
+
+	void Start() {
+		spring.spring = speed;
+		spring.damper = 1f;
+
+		if (!right)
+			maxAngle = -maxAngle;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey(KeyCode.RightShift))
+			spring.targetPosition = maxAngle;
+		else
+			spring.targetPosition = 0f;
+
+		hingeJoint.spring = spring;
+	}
+
+	void FixedUpdate() {
+
+	}
+}
---
+++ Assets/scripts/Table.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Table : MonoBehaviour {
+
+	// public only to be set in the Inspector
+	public float _angle = 8;
+
+	// Use this for initialization
+	void Start () {
+	
+		gameObject.transform.Rotate(new Vector3(-_angle, 0, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
