7753e2afd44a45b83515481eed4c30fc225991de
Zeno Rawling <zeno.rawling@gmail.com>
2014-02-02 01:17:29 +0000

    added a triangular bumper with mesh, physics mat, script and linked it to a GameManager to keep score

---
+++ Assets/scripts/Bumper.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bumper : MonoBehaviour {
+
+	public int points = 10;
+
+	GameManager _gameManager;
+
+	// Use this for initialization
+	void Start () {
+
+		_gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision collision) {
+
+		_gameManager.AddPoints(points);
+	}
+}
---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	int _score;
+	int _balls = 3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void AddPoints(int points) {
+		_score += points;
+		Debug.Log("Added " + points + "to total score upto " + _score);
+		OnGUI();
+	}
+
+	void OnGUI() {
+		GUI.Label(new Rect(10, 0, 100, 50), "Score: " + _score);
+	} 
+}
