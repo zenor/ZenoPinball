9ddc7d6980268f5213adf7ad0491646149232659
Zeno Rawling <zeno.rawling@gmail.com>
2014-02-02 02:58:06 +0000

    made the bumper into a proper prefab (todo: same for flipper), and added a scale tween oncollision

--- Assets/scripts/Bumper.cs
+++ Assets/scripts/Bumper.cs
@@ -4,22 +4,41 @@ using System.Collections;
 public class Bumper : MonoBehaviour {
 
 	public int points = 10;
+	public float scaleTo = 1.2f;
 
 	GameManager _gameManager;
+	Transform _meshTransform;
+	bool _scaling;
+	float _scaleRate = 0.05f;
 
 	// Use this for initialization
 	void Start () {
 
 		_gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+		_meshTransform = gameObject.transform.GetChild(0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		if (_scaling)
+		{
+			if (_meshTransform.localScale.x < 1f)
+				_scaleRate = Mathf.Abs(_scaleRate);
+			else if (_meshTransform.localScale.x > scaleTo)
+				_scaleRate = -Mathf.Abs(_scaleRate);
+
+			_meshTransform.localScale += Vector3.one * _scaleRate;
+
+			if (_meshTransform.localScale.x < 1f)
+				_scaling = false;
+		}
 	}
 
 	void OnCollisionEnter (Collision collision) {
 
 		_gameManager.AddPoints(points);
+		_scaling = true;
+		//_meshTransform.localScale = new Vector3(scaleTo, 1f, scaleTo);
 	}
 }
