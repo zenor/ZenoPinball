f1fa6b012f9904da58bfd1becdb6d2c48c9cffcd
Zeno Rawling <zeno.rawling@gmail.com>
2014-02-03 23:12:59 +0000

    Added support for a single Hi Score

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -3,18 +3,28 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+	const string PPKEY_HI_SCORE_1 = "HiScore1";
+
 	public GameObject scoreTextObject;
+	public GameObject hiScoreTextObject;
 	public GameObject plunger;
 
 	TextMesh _scoreText;
+	TextMesh _hiScoreText;
 
 	int _score;
+	int _hiScore;
 	int _ballsRemaining = 2;
 
 	// Use this for initialization
 	void Start () {
+		_hiScore = PlayerPrefs.GetInt(PPKEY_HI_SCORE_1, 0);
+
 		_scoreText = scoreTextObject.GetComponent<TextMesh>();
 		_scoreText.text = "Score: 0";
+
+		_hiScoreText = hiScoreTextObject.GetComponent<TextMesh>();
+		_hiScoreText.text = "Hi Score: " + _hiScore;
 	}
 	
 	// Update is called once per frame
@@ -32,11 +42,18 @@ public class GameManager : MonoBehaviour {
 		_ballsRemaining--;
 		if (_ballsRemaining >= 0)
 			StartCoroutine(ReloadBall());
+		else
+			GameOver();
 	}
 
 	public IEnumerator ReloadBall() {
 		yield return new WaitForSeconds(2);
 		plunger.GetComponent<Plunger>().Reload();
+	}
+
+	void GameOver() {
+		if (_score > _hiScore)
+			PlayerPrefs.SetInt(PPKEY_HI_SCORE_1, _score);
 	}
 
 	void OnGUI() {
