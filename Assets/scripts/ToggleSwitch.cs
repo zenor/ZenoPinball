5a479316b851ee794bb0535034715f686685ce81
Zeno Rawling <zeno.rawling@gmail.com>
2014-02-06 00:18:52 +0000

    Added a little section at the top in which to house the new feature of switches wot can be grouped and give bonus points if they are all active at once

---
+++ Assets/scripts/ToggleSwitch.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleSwitch : MonoBehaviour {
+
+	public float inactiveOffset = 0.07f;
+	public int points = 20;
+
+	GameManager _gameManager;
+	ToggleSwitchGroup _group;
+	bool _active = false;
+	Vector3 _offPos;
+	Vector3 _onPos;
+
+	// Use this for initialization
+	void Start () {
+		_gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+		_onPos = gameObject.transform.position;
+		_offPos = _onPos;
+		_offPos.y -= inactiveOffset;
+		gameObject.transform.position = _offPos;
+		_group = gameObject.transform.parent.GetComponent<ToggleSwitchGroup>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Toggle() {
+		_active = !_active;
+		_group.UpdateStates();
+		setPos();
+	}
+
+	public bool active {
+		get { return _active; }
+		set {
+			_active = value;
+			setPos();
+		}
+	}
+
+	void setPos() {
+		if (_active)
+			gameObject.transform.position = _onPos;
+		else
+			gameObject.transform.position = _offPos;
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		_gameManager.AddPoints(points);
+		Toggle();
+	}
+}
---
+++ Assets/scripts/ToggleSwitchGroup.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ToggleSwitchGroup : MonoBehaviour {
+
+	public int completePoints = 500;
+
+	GameManager _gameManager;
+	List<ToggleSwitch> _switches = new List<ToggleSwitch>();
+	List<bool> _currStates = new List<bool>();
+
+	// Use this for initialization
+	void Start () {
+		_gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+		foreach(ToggleSwitch ts in gameObject.GetComponentsInChildren<ToggleSwitch>()) {
+			_switches.Add(ts);
+			_currStates.Add(ts.active);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown(KeyCode.LeftShift)
+		    || Input.GetKeyDown(KeyCode.RightShift)) {
+			CycleToggleSwitches();
+		}
+	}
+
+	void CycleToggleSwitches() {
+
+
+		for (int i = 0; i < _switches.Count; ++i) {
+			if (i == 0)
+				_switches[i].active = _currStates[_switches.Count - 1];
+			else
+				_switches[i].active = _currStates[i - 1];
+		}
+
+		UpdateStates();
+	}
+
+	public void UpdateStates() {
+		bool isComplete = true;
+		for (int i = 0; i < _switches.Count; ++i) {
+			_currStates[i] = _switches[i].active;
+			if (!_currStates[i])
+				isComplete = false;
+		}
+		if (isComplete) {
+			_gameManager.AddPoints(completePoints);
+			for (int i = 0; i < _switches.Count; ++i) {
+				_switches[i].active = false;
+				_currStates[i] = false;
+			}
+		}
+	}
+}
