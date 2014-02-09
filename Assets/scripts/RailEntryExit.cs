3f1594dc89ac22cb767b628c8ab8b0fab755e4e1
Zeno Rawling <zeno.rawling@gmail.com>
2014-02-09 01:36:31 +0000

    added the rail/ramp

---
+++ Assets/scripts/RailEntryExit.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RailEntryExit : MonoBehaviour {
+
+	public GameObject playfieldTopCollider;
+	Collider _collider;
+
+	// Use this for initialization
+	void Start () {
+		_collider = playfieldTopCollider.GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		_collider.enabled = !_collider.enabled;
+	}
+}
