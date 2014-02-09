26562e7a69f5c5102759ce5a5e2dad7aa5153a8c
Zeno Rawling <zeno.rawling@gmail.com>
2014-02-09 14:18:58 +0000

    Refactored the points giving code for points objects into a base class and applied this to the Bumper class

--- Assets/scripts/Bumper.cs
+++ Assets/scripts/Bumper.cs
@@ -1,20 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Bumper : MonoBehaviour {
+public class Bumper : PointsGiver {
 
-	public int points = 10;
 	public float scaleTo = 1.2f;
 
-	GameManager _gameManager;
 	Transform _meshTransform;
 	bool _scaling;
 	float _scaleRate = 0.05f;
 
 	// Use this for initialization
 	void Start () {
-
-		_gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+		base.Start();
 		_meshTransform = gameObject.transform.GetChild(0);
 	}
 	
@@ -36,9 +33,7 @@ public class Bumper : MonoBehaviour {
 	}
 
 	void OnCollisionEnter (Collision collision) {
-
-		_gameManager.AddPoints(points);
+		AddPoints();
 		_scaling = true;
-		//_meshTransform.localScale = new Vector3(scaleTo, 1f, scaleTo);
 	}
 }
---
+++ Assets/scripts/PointsGiver.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class PointsGiver : MonoBehaviour {
+
+	public int points = 10;
+
+	GameManager _gameManager;
+
+	// Use this for initialization
+	protected void Start () {
+		_gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	protected void AddPoints() {
+		_gameManager.AddPoints(points);
+	}
+}
