49aa79c8428d36763db02715aa7ec77ca9c27fb9
Zeno Rawling <zeno.rawling@gmail.com>
2014-02-09 16:05:56 +0000

    added the rollover wires

---
+++ Assets/scripts/RolloverWire.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RolloverWire : PointsGiver {
+
+	// Use this for initialization
+	void Start () {
+		base.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		AddPoints();
+	}
+}
