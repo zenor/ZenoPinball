ef3268d3b0862f49f8aebbeae91db9cf84e38629
Zeno Rawling <zeno.rawling@gmail.com>
2014-02-10 00:47:52 +0000

    added quit button on escape key

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -28,6 +28,8 @@ public class GameManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+		if (Input.GetKeyUp(KeyCode.Escape))
+			Application.Quit();
 	}
 
 	public void AddPoints(int points) {
