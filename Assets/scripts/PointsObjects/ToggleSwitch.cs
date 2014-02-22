6f372f30ff9a7e0d1b786654e4d6d5a3eeed41e3
Zeno Rawling <zeno.rawling@gmail.com>
2014-02-22 20:13:27 +0000

    toggleswitches now change materials when they change state

--- Assets/scripts/PointsObjects/ToggleSwitch.cs
+++ Assets/scripts/PointsObjects/ToggleSwitch.cs
@@ -4,7 +4,11 @@ using System.Collections;
 public class ToggleSwitch : PointsGiver {
 
 	public float inactiveOffset = 0.07f;
+	
+	public Material offMaterial;
 
+	MeshRenderer _meshRenderer;
+	Material _onMaterial;
 	ToggleSwitchGroup _group;
 	bool _active = false;
 	Vector3 _offPos;
@@ -13,11 +17,18 @@ public class ToggleSwitch : PointsGiver {
 	// Use this for initialization
 	void Start () {
 		base.Start();
+		_meshRenderer = gameObject.GetComponent<MeshRenderer>();
 		_onPos = gameObject.transform.position;
 		_offPos = _onPos;
 		_offPos.y -= inactiveOffset;
+		_onMaterial = _meshRenderer.materials[0];
+
+		if (offMaterial) {
+			_meshRenderer.material = offMaterial;
+		}
 		gameObject.transform.position = _offPos;
 		_group = gameObject.transform.parent.GetComponent<ToggleSwitchGroup>();
+
 	}
 	
 	// Update is called once per frame
@@ -29,6 +40,7 @@ public class ToggleSwitch : PointsGiver {
 		_active = !_active;
 		_group.UpdateStates();
 		setPos();
+		setMat ();
 	}
 
 	public bool active {
@@ -36,6 +48,7 @@ public class ToggleSwitch : PointsGiver {
 		set {
 			_active = value;
 			setPos();
+			setMat();
 		}
 	}
 
@@ -44,6 +57,12 @@ public class ToggleSwitch : PointsGiver {
 			gameObject.transform.position = _onPos;
 		else
 			gameObject.transform.position = _offPos;
+	}
+
+	void setMat() {
+		if (offMaterial) {
+			_meshRenderer.material = _active ? _onMaterial : offMaterial;
+		}
 	}
 
 	void OnTriggerEnter(Collider collider) {
