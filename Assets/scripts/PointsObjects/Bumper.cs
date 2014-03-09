9f21bb2640720f0dc17c3526dc099e8b6b073de2
Zeno Rawling <zeno.rawling@gmail.com>
2014-03-09 18:10:44 +0000

    Refactored the bumpers kicker force out into its own component for reuse on the breakout block

--- Assets/scripts/PointsObjects/Bumper.cs
+++ Assets/scripts/PointsObjects/Bumper.cs
@@ -5,7 +5,6 @@ public class Bumper : PointsGiver {
 
 	public float scaleTo = 1.2f;
 	public float cameraShake = 0.008f;
-	public float kickerForce = 100f;
 
 	CameraTricks _camera;
 
@@ -42,13 +41,6 @@ public class Bumper : PointsGiver {
 			AddPoints();
 			_camera.Shake(cameraShake);
 			_scaling = true;
-
-			if (collision.rigidbody.velocity.magnitude < Constants.Ball.MaxVelocity) {
-				Debug.Log("ball velocity: " + collision.rigidbody.velocity.magnitude);
-				float x = collision.rigidbody.velocity.x > 0 ? kickerForce : -kickerForce;
-				float z = collision.rigidbody.velocity.z > 0 ? kickerForce : -kickerForce;
-				collision.rigidbody.AddForce(new Vector3(x, 0f, z));
-			}
 		}
 	}
 }
---
+++ Assets/scripts/TableObjects/Kicker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kicker : MonoBehaviour {
+
+	public float Force = 100f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		if (collision.transform.tag == "Ball") {			
+			if (collision.rigidbody.velocity.magnitude < Constants.Ball.MaxVelocity) {
+				float x = collision.rigidbody.velocity.x > 0 ? Force : -Force;
+				float z = collision.rigidbody.velocity.z > 0 ? Force : -Force;
+				collision.rigidbody.AddForce(new Vector3(x, 0f, z));
+			}
+		}
+	}
+}
