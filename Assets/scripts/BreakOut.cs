2e19f88fc3eccebf3236ca25dbc2b25f70fe43f9
Zeno Rawling <zeno.rawling@gmail.com>
2014-03-09 19:00:42 +0000

    breakout mode can now be ended and reset

--- Assets/scripts/BreakOut.cs
+++ Assets/scripts/BreakOut.cs
@@ -10,6 +10,7 @@ public class BreakOut : MonoBehaviour, ITriggerable, IResetable {
 
 	public int blocksHoriz = 8;
 	public int blocksVert = 8;
+	BreakOutBlock[] blocks;
 
 	const float xPos = -3.5f;
 
@@ -31,12 +32,17 @@ public class BreakOut : MonoBehaviour, ITriggerable, IResetable {
 		BreakoutMode.transform.localPosition = Vector3.zero;
 		BreakoutMode.transform.rotation = transform.rotation;
 
-		for (int x = 0; x < blocksHoriz; ++x) {
-			for (int z = 0; z < blocksVert; ++z) {
+		blocks = new BreakOutBlock[blocksHoriz * blocksVert];
+
+		for (int z = 0; z < blocksVert; ++z) {
+			for (int x = 0; x < blocksHoriz; ++x) {
 				Transform newBlock = (Transform)Instantiate(Block);
 				newBlock.parent = BreakoutMode.transform;
 				newBlock.localPosition = new Vector3(x * 1f, BreakoutMode.transform.position.y + 0.2f, z * 1f);
 				newBlock.transform.rotation = BreakoutMode.transform.rotation;
+
+				int index = z * blocksHoriz + x;
+				blocks[index] = newBlock.GetComponent<BreakOutBlock>();
 			}
 		}
 		transform.localPosition = new Vector3(xPos, transform.position.y, transform.position.z);
@@ -45,11 +51,20 @@ public class BreakOut : MonoBehaviour, ITriggerable, IResetable {
 	}
 
 	public void Trigger() {
-		MainGameMode.SetActive(false);
-		BreakoutMode.SetActive(true);
+		if (!BreakoutMode.activeInHierarchy) {
+			MainGameMode.SetActive(false);
+			BreakoutMode.SetActive(true);
+		} else {
+			Reset();
+			MainGameMode.SetActive(true);
+		}
+
 	}
 
 	public void Reset() {
-
+		foreach(BreakOutBlock block in blocks) {
+			block.gameObject.SetActive(true);
+		}
+		BreakoutMode.SetActive(false);
 	}
 }
