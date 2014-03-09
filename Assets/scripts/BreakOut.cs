3d0a65d024a348530bd5de52e5359df1e2be4754
Zeno Rawling <zeno.rawling@gmail.com>
2014-03-09 17:42:31 +0000

    basics of BreakOut Bonus mode. still todo: reseting, bonus over condition, polish intro and outro, polish breaking blocks

--- Assets/scripts/BreakOut.cs
+++ Assets/scripts/BreakOut.cs
@@ -1,21 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BreakOut : MonoBehaviour, ITriggerable {
+public class BreakOut : MonoBehaviour, ITriggerable, IResetable {
 
 	public GameObject MainGameMode;
+	public Transform Block;
+
+	GameObject BreakoutMode;
+
+	public int blocksHoriz = 8;
+	public int blocksVert = 8;
+
+	const float xPos = -3.5f;
 
 	// Use this for initialization
 	void Start () {
-	
+		Setup();	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// for debug
+		if (Input.GetKeyUp(KeyCode.B))
+			Trigger();
+	}
+
+	void Setup() {
+		BreakoutMode = new GameObject("Blocks");
+		BreakoutMode.transform.parent = transform;
+		BreakoutMode.transform.localPosition = Vector3.zero;
+		BreakoutMode.transform.rotation = transform.rotation;
+
+		for (int x = 0; x < blocksHoriz; ++x) {
+			for (int z = 0; z < blocksVert; ++z) {
+				Transform newBlock = (Transform)Instantiate(Block);
+				newBlock.parent = BreakoutMode.transform;
+				newBlock.localPosition = new Vector3(x * 1f, BreakoutMode.transform.position.y + 0.2f, z * 1f);
+				newBlock.transform.rotation = BreakoutMode.transform.rotation;
+			}
+		}
+		transform.localPosition = new Vector3(xPos, transform.position.y, transform.position.z);
+
+		BreakoutMode.SetActive(false);
 	}
 
 	public void Trigger() {
 		MainGameMode.SetActive(false);
+		BreakoutMode.SetActive(true);
+	}
+
+	public void Reset() {
+
 	}
 }
---
+++ Assets/scripts/PointsObjects/BreakOutBlock.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakOutBlock : PointsGiver {
+
+	// Use this for initialization
+	void Start () {
+		base.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		if (collision.transform.tag == "Ball") {
+			AddPoints();
+			gameObject.SetActive(false);
+		}
+	}
+}
